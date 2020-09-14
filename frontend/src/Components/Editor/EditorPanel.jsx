0f15ba2da2748e72b50612e35130b150bc359093
Mehran <imganjgahi@gmail.com>
2020-09-15 01:17:37 +0430

    dragable EditorPanel

--- frontend/src/Components/Editor/EditorPanel.jsx
+++ frontend/src/Components/Editor/EditorPanel.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
+import { useDragging } from '../Utils/Dragable';
 
 const EditorPanel = (props) => {
+    const [ref, x, y, isDragging] = useDragging();
     return ( 
-        <div className="editorPanel">
+        <div className="editorPanel"
+        ref={ref}
+        style={{
+            left: x,
+            top: y
+        }}
+        >
             <div className="panelHeader">
                 <span>Panel</span>
                 <span onClick={props.onClose}>X</span>
