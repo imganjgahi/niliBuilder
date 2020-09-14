e3de50b4ae081c459cc1fc81a5e99ff2deae8dd4
Mehran <imganjgahi@gmail.com>
2020-09-15 00:31:39 +0430

    template page

--- frontend/src/Pages/Temp.jsx
+++ frontend/src/Pages/Temp.jsx
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useParams } from 'react-router';
+import EditorPanel from '../Components/Editor/EditorPanel';
+import RenderEngin from '../Components/Editor/RenderEngine';
+import { TEMPLATES } from '../Data/Templates';
 
 const Temp = (props) => {
     let {tempId} = useParams()
+    const [showPanel, setPanelStatus] = useState(false);
+    const [targetElement, setTargetElement] = useState(null);
+
+    function closePanel() {
+        setTargetElement(null);
+        setPanelStatus(false);
+    }
+
+    function selectedElementHandler(el) {
+        console.log(el)
+    }
     return ( 
-        <p>
-           Temp: {tempId}
-        </p>
+        <>
+        <RenderEngin template={TEMPLATES.filter(x => x.id === tempId)[0]} onSelectElement={(element) => selectedElementHandler(element)} />
+        {showPanel && targetElement && <EditorPanel onClose={closePanel} /> }
+        </>
      );
 }
  
