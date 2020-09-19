3ea763bf097daf074bb8d1295d3c7439e56d5715
Mehran <imganjgahi@gmail.com>
2020-09-19 05:48:57 -0700

    Update template after change its children styles

--- frontend/src/Pages/Temp.jsx
+++ frontend/src/Pages/Temp.jsx
@@ -6,6 +6,7 @@ import { TEMPLATES } from '../Data/Templates';
 
 const Temp = (props) => {
     let {tempId} = useParams()
+    const [templates, setTemplates] = useState(TEMPLATES)
     const [showPanel, setPanelStatus] = useState(false);
     const [targetElement, setTargetElement] = useState(null);
 
@@ -19,13 +20,43 @@ const Temp = (props) => {
         setTargetElement(el);
         setPanelStatus(true);
     }
+
+    function returnUpdatedElement(el, updatedEl) {
+
+        if(el.id === updatedEl.id){
+            return updatedEl
+        }
+
+        if(el.children) {
+            el.children = el.children.map(item => returnUpdatedElement(item, updatedEl))
+        }
+
+        return el
+    }
+    function updateTemplates (updatedElement) {
+        return templates.map(item => returnUpdatedElement(item, updatedElement))
+    }
+    function styleChangeHandler(value) {
+        const updatedElement = {
+            ...targetElement,
+            style: value
+        }
+        setTemplates(updateTemplates(updatedElement))
+        console.log("styleChangeHandler: ", updatedElement)
+    }
+    if(!templates) {
+        return <p> LOADING ...</p>
+    }
     return ( 
         <>
         <RenderEngin 
-        template={TEMPLATES.filter(x => x.id === tempId)[0]} 
+        template={templates.filter(x => x.id === tempId)[0]} 
         onSelectElement={selectedElementHandler} />
+
+
         {showPanel && targetElement && <EditorPanel 
         onClose={closePanel} 
+        onStyleChange={styleChangeHandler}
         el={targetElement} /> }
         </>
      );
